874ac04c47334727aca6f77a732212481c7c9f20
GBTC440010UR <GBTC440010UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-09 19:09:44 -0400

    Initial commit

---
+++ src/main/java/com/thomas/firstprogram/HomeController.java
@@ -0,0 +1,13 @@
+package com.thomas.firstprogram;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController
+{
+    @RequestMapping("/")
+    public String homePage()
+    {
+        return "index";
+    }
+}
